d0e6aac2f4574461ecc0aad541e5c90df134dcc4
Rajeshroulo <rajraval017@gmail.com>
2020-05-19 23:07:35 +0530

    Added some test cases for IPL most runs Data

--- src/test/java/cricketleagueanalyser/CricketLeagueAnalyserTest.java
+++ src/test/java/cricketleagueanalyser/CricketLeagueAnalyserTest.java
@@ -6,7 +6,12 @@ import org.junit.Test;
 public class CricketLeagueAnalyserTest {
 
     private static final String IPL_MOSTRUNS_DATA="src\\test\\resources\\IPL2019FactsheetMostRuns.csv";
-    private static final String IPL_MOSTWKTS_DATA="src\\test\\resources\\IPL2019FactsheetMostWkts.csv";
+    private static final String IPL_MOSTRUNS_DATA_WRONG_PATH ="src\\main\\resources\\IPL2019FactsheetMostRuns.csv";
+    private static final String IPL_MOSTRUNS_DATA_WRONG_DELIMITER = "src\\test\\resources\\IPL2019FactsheetMostRunsWithWrongDelimiter.csv";
+    private static final String IPL_MOSTRUNS_DATA_WRONG_HEADER ="src\\test\\resources\\IPL2019FactsheetMostRunsWithWrongHeader.csv";
+    private static final String IPL_MOSTRUNS_DATA_WRONG_TYPE ="src\\test\\resources\\IPL2019FactsheetMostRuns.txt";;
+    private static final String IPL_MOSTRUNS_DATA_EMPTY_FILE ="src\\test\\resources\\IPL2019FactsheetMostRunsEmptyFile.csv";;
+
 
     @Test
     public void givenIPLMostRunsData_shouldReturnTotalNumberOfPlayers() {
@@ -16,5 +21,56 @@ public class CricketLeagueAnalyserTest {
             Assert.assertEquals(100,numOfRecords);
         } catch (CricketLeagueAnalyserException e) {}
     }
+
+    @Test
+    public void givenIPLMOstRunsData_WithWrongPath_ShouldThrowException() {
+        try {
+            CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
+            cricketLeagueAnalyser.loadCricketLeagueData(IPL_MOSTRUNS_DATA_WRONG_PATH);
+        } catch (CricketLeagueAnalyserException e) {
+            Assert.assertEquals(CricketLeagueAnalyserException.ExceptionType.IPL_FILE_PROBLEM,e.type);
+        }
+    }
+
+    @Test
+    public void givenIPLMostRunsData_WhenEmpty_ShouldThrowException() {
+        try {
+            CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
+            cricketLeagueAnalyser.loadCricketLeagueData(IPL_MOSTRUNS_DATA_EMPTY_FILE);
+        } catch (CricketLeagueAnalyserException e) {
+            Assert.assertEquals(CricketLeagueAnalyserException.ExceptionType.NO_DATA,e.type);
+        }
+    }
+
+    @Test
+    public void givenIPLMostRunsData_WithWrongType_ShouldThrowException() {
+        try {
+            CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
+            cricketLeagueAnalyser.loadCricketLeagueData(IPL_MOSTRUNS_DATA_WRONG_TYPE);
+        } catch (CricketLeagueAnalyserException e) {
+            Assert.assertEquals(CricketLeagueAnalyserException.ExceptionType.UNABLE_TO_PARSE,e.type);
+        }
+    }
+
+    @Test
+    public void givenIPLMostRunsData_WithWrongHeader_ShouldThrowException() {
+        try {
+            CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
+            cricketLeagueAnalyser.loadCricketLeagueData(IPL_MOSTRUNS_DATA_WRONG_HEADER);
+        } catch (CricketLeagueAnalyserException e) {
+            Assert.assertEquals(CricketLeagueAnalyserException.ExceptionType.UNABLE_TO_PARSE,e.type);
+        }
+    }
+
+    @Test
+    public void givenIPLMostRunsData_WithWrongDelimiter_ShouldThrowException() {
+        try {
+            CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
+            cricketLeagueAnalyser.loadCricketLeagueData(IPL_MOSTRUNS_DATA_WRONG_DELIMITER);
+        } catch (CricketLeagueAnalyserException e) {
+            Assert.assertEquals(CricketLeagueAnalyserException.ExceptionType.UNABLE_TO_PARSE,e.type);
+        }
+    }
+
 
 }
